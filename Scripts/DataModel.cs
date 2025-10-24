a2152b293720add864f4803ed656ccbff9d4cc06
The_Universality <the_universality@proton.me>
2025-10-24 11:49:36 +0200

    Functions

--- Scripts/DataModel.cs
+++ Scripts/DataModel.cs
@@ -10,6 +10,14 @@ public partial class DataModel : Node2D
 
 	public void F_SanityChange_RNil(int PAR_Sanity)
 	{
-		GetTree().Root.SetMeta("Sanity", (int)GetTree().Root.GetMeta("Sanity")+PAR_Sanity);
+		if(!(PAR_Sanity+(int)GetTree().Root.GetMeta("Sanity") > 100))
+		{
+			GetTree().Root.SetMeta("Sanity", (int)GetTree().Root.GetMeta("Sanity")+PAR_Sanity);
+		}
+	}
+
+	public void F_ChangeLevel_RNil()
+	{
+
 	}
 }
