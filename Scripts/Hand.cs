2a49add9920fdd2e2d4c4e489b72de67715f7aa9
Hanus Valenta <hanus.valenta@gmail.com>
2025-10-24 12:47:58 +0200

    hand move

--- Scripts/Hand.cs
+++ Scripts/Hand.cs
@@ -3,4 +3,18 @@ using System;
 
 public partial class Hand : Sprite2D
 {
+	public override void _Ready()
+	{
+		Input.MouseMode = Input.MouseModeEnum.Hidden;
+	}
+
+	public override void _Process(double delta)
+	{
+		GlobalPosition = GetGlobalMousePosition();
+	}
+
+	public override void _ExitTree()
+	{
+		Input.MouseMode = Input.MouseModeEnum.Visible;
+	}
 }
