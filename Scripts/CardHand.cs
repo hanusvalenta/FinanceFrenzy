a7b468ddad597d3a73c9015f4fb6d2d0cee0f699
Hanus Valenta <hanus.valenta@gmail.com>
2025-10-24 21:03:32 +0200

    Win con

--- Scripts/CardHand.cs
+++ Scripts/CardHand.cs
@@ -3,15 +3,42 @@ using System;
 
 public partial class CardHand : Sprite2D
 {
+	private bool _isXLocked = false;
+	private const float LOCK_X_POSITION = 661.0f;
+
+	private Vector2 _previousMousePosition;
+	private const float SWIPE_VELOCITY_THRESHOLD = 2500.0f;
+	private bool _hasWon = false;
+
 	public override void _Ready()
 	{
 		Input.MouseMode = Input.MouseModeEnum.Hidden;
+		_previousMousePosition = GetGlobalMousePosition();
 	}
 
 	public override void _Process(double delta)
 	{
 		Vector2 mousePosition = GetGlobalMousePosition();
-		float newX = Mathf.Min(mousePosition.X, 662.0f);
+		float newX;
+
+		if (!_hasWon && delta > 0)
+		{
+			float velocityY = (mousePosition.Y - _previousMousePosition.Y) / (float)delta;
+			if (velocityY > SWIPE_VELOCITY_THRESHOLD)
+			{
+				GD.Print("Win");
+				_hasWon = true;
+			}
+		}
+		_previousMousePosition = mousePosition;
+
+		if (mousePosition.X >= LOCK_X_POSITION)
+		{
+			_isXLocked = true;
+		}
+
+		newX = _isXLocked ? LOCK_X_POSITION : mousePosition.X;
+
 		GlobalPosition = new Vector2(newX, mousePosition.Y);
 	}
 
