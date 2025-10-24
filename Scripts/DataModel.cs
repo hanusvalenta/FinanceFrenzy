18b06d40aa1312b0dc70d29d4035fbfd42aad206
The_Universality <the_universality@proton.me>
2025-10-24 11:23:06 +0200

    Bound signals

---
+++ Scripts/DataModel.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public partial class DataModel : Node2D
+{
+	public override void _Ready()
+	{
+		GD.Print(GetTree().Root.GetMeta("Sanity"));
+	}
+
+	public void F_SanityChange_RNil(int PAR_Sanity)
+	{
+		GetTree().Root.SetMeta("Sanity", (int)GetTree().Root.GetMeta("Sanity")+PAR_Sanity);
+	}
+}
