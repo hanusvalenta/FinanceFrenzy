7bb587692ce7b74a226bb7c977d8b632eaa90bc3
The_Universality <the_universality@proton.me>
2025-10-24 16:16:51 +0200

    Menu music working

--- Scripts/SimpleMusicManager.cs
+++ Scripts/SimpleMusicManager.cs
@@ -5,37 +5,21 @@ public partial class SimpleMusicManager : Node
 [Export] private AudioStreamWav HelloSound;
 	[Export] private AudioStreamWav MenuMusic;
 
-	private AudioStreamPlayer _helloPlayer;
-	private AudioStreamPlayer _menuPlayer;
+	private AudioStreamPlayer _AudioPlayer;
 
 	public override void _Ready()
 	{
 		// Audio pro hello
-		_helloPlayer = new AudioStreamPlayer();
-		_helloPlayer.Stream = HelloSound;
-		AddChild(_helloPlayer);
+		_AudioPlayer		= new AudioStreamPlayer();
+		_AudioPlayer.Stream = HelloSound;
+		AddChild(_AudioPlayer);
 
-		// Audio pro menu hudbu
-		_menuPlayer = new AudioStreamPlayer();
-		if (MenuMusic != null)
+		_AudioPlayer.Play();
+
+		_AudioPlayer.Finished	+= () =>
 		{
-			// Nastavení smyčky přímo na streamu
-			MenuMusic.LoopMode = AudioStreamWav.LoopModeEnum.Forward;
-			_menuPlayer.Stream = MenuMusic;
-		}
-		AddChild(_menuPlayer);
-
-		// Finished signál pro hello
-		_helloPlayer.Finished += OnHelloFinished;
-
-		// Spusť hello
-		_helloPlayer.Play();
-		GD.Print("Přehrávám hello...");
-	}
-
-	private void OnHelloFinished()
-	{
-		GD.Print("Hello skončilo, start menu hudby...");
-		_menuPlayer.Play();
+			_AudioPlayer.Stream	= MenuMusic;
+			_AudioPlayer.Play();
+		};
 	}
 }
